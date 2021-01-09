778e354701c213dfc3decc12293ae9099c8a4bd5
Muthufucah_5 <corneezius@Muthufucah-5s-iMac.local>
2021-01-08 20:04:11 -0500

    udpate responsive contributor logic

--- js/filter-project.js
+++ js/filter-project.js
@@ -17,3 +17,31 @@ $(".filter-projects-dropdown a").on("click", function(e) {
         $("." + selectedName).slideToggle();
     }
 });
+
+
+// dropdown clicked
+$(".filter-contributor-dropdown a").on("click", function (e) {
+    // preventing default behaviour of a tag
+    e.preventDefault();
+    // getting name of this selected item from dropdown
+    var selectedNameC = String($(this).find('.dropdown_name_contributor').text());
+    // setting name of button = name of selected item from dropdown
+    $(".filter-projects-btn-cont").text(selectedNameC);
+    // hiding all the projects.
+    selectedNameC = selectedNameC.toLowerCase();
+    // IMPORTANT: If new owner is added as class and in dropdown then it's name should be added here
+    $(".corneliusTeam,.CorneliusTeam,.omarTeam,.chrisTeam").fadeOut();
+    $(".Cornelius,.cornelius,.Omar,.Chris, .Rumble, .Podcasts").fadeOut();
+    // if show all pressed then show all
+    if (selectedNameC == "total") {
+        $(".corneliusTeam,.CorneliusTeam,.omarTeam,.chrisTeam").slideToggle();
+    }
+    else if (selectedNameC == 'cornelius') {
+        $(".corneliusTeam,.CorneliusTeam").slideToggle();
+    }
+    else {
+        // showing the selected owners
+        selectedNameC = selectedNameC + "Team";
+        $("." + selectedNameC).slideToggle();
+    }
+});
